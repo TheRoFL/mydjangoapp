7ae73dc07fe65129b0b7b5dbebfe5b5e6f7047ce
TheRoFL <gabellaoff@yandex.ru>
2023-05-03 21:54:22 +0500

    forth commit

--- Chat/models.py
+++ Chat/models.py
@@ -5,9 +5,10 @@ class Chat(models.Model):
     member_one = models.ForeignKey(ProfileData, on_delete = models.CASCADE, blank=False, null=False, related_name = 'member_one')
     member_two = models.ForeignKey(ProfileData, on_delete = models.CASCADE, blank=False, null=False, related_name = 'member_two')
     chat_avatar = models.ImageField(upload_to='chats/avatars', blank=True, null=True)
+    created_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.member_one + ', ' + self.member_two
+        return self.member_one.user.username + ', ' + self.member_two.user.username
 
 
 
@@ -16,7 +17,10 @@ class Message(models.Model):
     chat = models.ForeignKey(Chat, on_delete = models.CASCADE, blank=True, null=True)
     message = models.CharField(max_length=500, blank=False, null=False)
     uploaded_image = models.FileField(upload_to='chats/message_pictures', blank=True, null=True)
+    sent_time = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
-        return self.message[:20] + '...'
+        if len(self.message > 20): return self.message[:20] + '...'
+        else: return self.message
+
